127e6b4464c00733202eca375675d9ca26cc4516
JerryMata <matajerry951@gmail.com>
2024-11-30 09:00:09 +0700

    Create MenuItem.java

---
+++ MenuItem.java
@@ -0,0 +1,19 @@
+public abstract class MenuItem {
+    private String nama;
+    private double harga;
+
+    public MenuItem(String nama, double harga) {
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+    public abstract double hitungHarga(); 
+    public abstract void persiapkan(); // Abstraction
+}
